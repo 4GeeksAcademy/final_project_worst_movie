89e52ff6812177e085cd7e84902e1ffbc9f5ad98
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-09-09 22:53:05 +0000

    code almost done

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -7,9 +7,10 @@ export const Login = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-        <form>
+        <form className="text-center">
         <h1 className="text-light">Wellcome  to Your Best Worst Movie Log in.</h1>
-        <div className="formWrappa">
+        <div className="superFormWrappa ">
+        <div className="formWrappa ">
         <div className="mb-3">
           <label for="exampleInputEmail1" className="form-label text-warning">Email address</label>
           <input type="email" className="form-control" placeholder="Type your email adress here" id="exampleInputEmail1" aria-describedby="emailHelp"/>
@@ -30,6 +31,7 @@ export const Login = () => {
         <input type="email" className="form-control"
          placeholder="Typer here your email adress to receive a resset link" 
         id="exampleInputEmail1" aria-describedby="emailHelp"/>
+         <button type="submit" className="btn btn-primary">Send link</button>
          <p className="text-danger">
          Guess what? You're in the 'Not Registered' club! Time for a wild
           adventure—off to the registration form you go!
@@ -38,7 +40,8 @@ export const Login = () => {
         <button type="submit" className="btn btn-primary">Sign up.</button>
         </div>
        </div>
-      </form>
+        </div>
+        </form>
        
        
          );
